a57183610389767729171369b54ece8331b5d4a0
ff-cat <fatflycat@gmail.com>
2021-06-11 21:45:39 +0300

    added the ability to filter and delete tasks

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,44 @@
-import React from 'react';
+import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
 
+export type FilterType = 'all' | 'active' | 'completed'
+
 function App() {
 
-    const tasks1 = [
+    let [tasks, setTasks] = useState([
         {id: 1, title: 'HTML&CSS', isDone: true},
         {id: 2, title: 'JS', isDone: true},
-        {id: 3, title: 'React', isDone: false}
-    ]
-    const tasks2 = [
-        {id: 1, title: 'Milk', isDone: true},
-        {id: 2, title: 'Bread', isDone: false},
-        {id: 3, title: 'Meat', isDone: false}
-    ]
+        {id: 3, title: 'React', isDone: false},
+        {id: 4, title: 'Rest API', isDone: false}
+    ])
+
+    let [filter, setFilter] = useState<FilterType>('all')
+    let tasksForTodolist = tasks
+
+    if (filter === 'active') {
+        tasksForTodolist = tasks.filter(t => t.isDone === false)
+    } else if (filter === 'completed') {
+        tasksForTodolist = tasks.filter(t => t.isDone === true)
+    } else tasksForTodolist = tasks
+
+    function removeTask(id: number) {
+        let filteredTasks = tasks.filter(t => t.id != id)
+        setTasks(filteredTasks)
+    }
+
+    function changeFilter(value: FilterType) {
+            setFilter(value)
+    }
 
 
     return (
         <div className="App">
-            <Todolist title={'What to learn'} tasks={tasks1}/>
-            <Todolist title={'What to buy'} tasks={tasks2}/>
+            <Todolist title={'What to learn'}
+                      tasks={tasksForTodolist}
+                      removeTask={removeTask}
+                      changeFilter={changeFilter}
+            />
         </div>
     );
 }
