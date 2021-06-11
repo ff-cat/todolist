7ee13600a233573d6aa465c07960c0cb825ffab7
ff-cat <fatflycat@gmail.com>
2021-06-11 19:36:31 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import './App.css';
+import {Todolist} from "./Todolist";
+
+function App() {
+
+    const tasks1 = [
+        {id: 1, title: 'HTML&CSS', isDone: true},
+        {id: 2, title: 'JS', isDone: true},
+        {id: 3, title: 'React', isDone: false}
+    ]
+    const tasks2 = [
+        {id: 1, title: 'Milk', isDone: true},
+        {id: 2, title: 'Bread', isDone: false},
+        {id: 3, title: 'Meat', isDone: false}
+    ]
+
+
+    return (
+        <div className="App">
+            <Todolist title={'What to learn'} tasks={tasks1}/>
+            <Todolist title={'What to buy'} tasks={tasks2}/>
+        </div>
+    );
+}
+
+export default App;
