19af26f0d3b75f289de220d9c283da244fa9115d
ff-cat <fatflycat@gmail.com>
2021-06-12 22:01:31 +0300

    added the ability to add tasks and code refactoring

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,40 @@
 import React, {useState} from 'react';
 import './App.css';
 import {Todolist} from "./Todolist";
+import {v1} from "uuid";
 
 export type FilterType = 'all' | 'active' | 'completed'
 
 function App() {
 
     let [tasks, setTasks] = useState([
-        {id: 1, title: 'HTML&CSS', isDone: true},
-        {id: 2, title: 'JS', isDone: true},
-        {id: 3, title: 'React', isDone: false},
-        {id: 4, title: 'Rest API', isDone: false}
+        {id: v1(), title: 'HTML&CSS', isDone: true},
+        {id: v1(), title: 'JS', isDone: true},
+        {id: v1(), title: 'React', isDone: false},
+        {id: v1(), title: 'Rest API', isDone: false}
     ])
-
     let [filter, setFilter] = useState<FilterType>('all')
+
     let tasksForTodolist = tasks
 
     if (filter === 'active') {
-        tasksForTodolist = tasks.filter(t => t.isDone === false)
+        tasksForTodolist = tasks.filter(t => !t.isDone)
     } else if (filter === 'completed') {
-        tasksForTodolist = tasks.filter(t => t.isDone === true)
+        tasksForTodolist = tasks.filter(t => t.isDone)
     } else tasksForTodolist = tasks
 
-    function removeTask(id: number) {
+    function removeTask(id: string) {
         let filteredTasks = tasks.filter(t => t.id != id)
         setTasks(filteredTasks)
     }
-
     function changeFilter(value: FilterType) {
             setFilter(value)
+    }
+    function addTask(value: string) {
+        let newTask = {id: v1(), title: value, isDone: false}
+        let newTasks = [newTask, ...tasks]
+        setTasks(newTasks)
+
     }
 
 
@@ -38,6 +44,7 @@ function App() {
                       tasks={tasksForTodolist}
                       removeTask={removeTask}
                       changeFilter={changeFilter}
+                      addTask={addTask}
             />
         </div>
     );
